b5df1ab3de34bc2ed962a5680eb60ea7e17c5f71
Hugh Kennedy <hughskennedy@gmail.com>
2013-10-09 11:55:17 +1100

    improvements/fixes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var combine = require('stream-combiner')
 var glslify = require('glslify-stream')
 var concat = require('concat-stream')
 var map = require('map-stream')
+var fs = require('graceful-fs')
 var split = require('split')
 var eol = require('os').EOL
-var fs = require('fs')
 
 module.exports = include
 
@@ -14,10 +14,11 @@ function include(file) {
   return stream
 
   function write(line, next) {
+    var cache = {}
     var match
 
     line = String(line)
-    line = line.replace(/#pragma\s+import\:(.+)$/g, function(full, name) {
+    line = line.replace(/^\s*?#pragma\s+import\:(.+)$/g, function(full, name) {
       match = name.trim()
       return ''
     })
@@ -26,16 +27,21 @@ function include(file) {
 
     glslify.resolve(file, match, function(err, dest) {
       if (err) return stream.emit('error', err)
+      if (cache[dest]) return next(null, cache[dest])
 
       // Handle imports recursively too!
       fs.createReadStream(dest)
+        .on('error', error)
         .pipe(include(dest))
+        .on('error', error)
         .pipe(concat(function(data) {
-          next(null, data + eol)
+          next(null, cache[dest] = data + eol)
         }))
-        .on('error', function(err) {
-          return stream.emit('error', err)
-        })
+        .on('error', error)
+
+      function error(err) {
+        return stream.emit('error', err)
+      }
     })
   }
 }
